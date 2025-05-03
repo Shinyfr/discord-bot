61dd4ca4f0a7dafe72e494ed2f5383b0fb1d3d85
Shiny <shinyy031@gmail.com>
2025-05-03 13:01:08 +0200

    Ajoute l'exécution des commandes slash

--- index.js
+++ index.js
@@ -65,5 +65,29 @@ client.on('interactionCreate', async interaction => {
   }
 });
 
+const fs = require('fs');
+const path = require('path');
+const { Collection, EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.warn(`[AVERTISSEMENT] La commande à ${filePath} est invalide.`);
+    }
+  }
+}
+
+
 
 client.login(process.env.TOKEN);
